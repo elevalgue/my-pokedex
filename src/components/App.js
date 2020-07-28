e2a2d68e90fe456e11a84f0f200c83672cf1333b
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-07-28 09:30:06 +0200

    add pokemon json

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> Hola mundo!
         </p>
         <a
           className='App-link'
